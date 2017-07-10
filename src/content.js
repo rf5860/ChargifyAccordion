da021470b06d70158a567a83c3694a6807103c67
Robert Foy <robert.foy@gmail.com>
2017-07-10 16:55:59 +1000

    Collapse by default

--- src/content.js
+++ src/content.js
@@ -1,3 +1,4 @@
 const collapsibleElements = '.product_family_collection_heading, .product_family_heading'
 $(collapsibleElements).click(function() {$(this).siblings().slideToggle(500)})
+$('.product_family_heading').siblings().slideToggle(1)
 
